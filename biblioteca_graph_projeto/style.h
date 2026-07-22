fc33a3857acb7cbc75194d9438d87b211d51bc1d
João V. R. da Silva <joaovictor.rodrigues.contact@gmail.com>
2026-07-22 12:10:39 -0300

    Tratamento de erros
    Agora é possível verificar se o id que o usuário insere (quando é pedido) é válido ou não. Adaptações no main.c foram realizadas para que isso pudesse ocorrer.

--- biblioteca_graph_projeto/style.h
+++ biblioteca_graph_projeto/style.h
@@ -1,14 +1,11 @@
 #include "Clic.h"
+#include "Graph.h"           // Biblioteca importante para as duas últimas funções
+#include "mecanismo_busca.h" // Biblioteca importante para as duas últimas funções
 
-// BIBLIOTECAS IMPORTANTES PARA A PENÚLTIMA FUNÇÃO
-#include "Graph.h"
-#include "mecanismo_busca.h"
-
-#define MAX_OPTIONS    9  // total de índices no vetor de strings do menu
-#define LEAVE_OPTION   9  // respectivo índice do vetor (para saída) somado a 1; index = i + 1
-#define SEARCH_OPTION  6  // respectivo índice do vetor (para consulta/pesquisa) somado a 1; index = i + 1
-#define NOT_FINISHED  -1
-#define FINISHED       1
+#define MAX_OPTIONS    8  // total de índices no vetor de strings do menu
+#define LEAVE_OPTION   8  // respectivo índice do vetor (para saída) somado a 1; index = i + 1
+#define OKAY           1
+#define NOT_OKAY       0
 
 typedef enum Say{
     // Discrimina uma variável do tipo inteiro
@@ -27,14 +24,27 @@ typedef enum Say{
     Say_PESOsite  = 8,
 }Say;
 
+typedef enum State {
+    ERROR = 0,
+    WAIT = 1,
+    SUCCESS = 2,
+    OTHER_SCREEN = 3,
+}State;
+
+// Verifica se o id é válido
+int Style_idVerify(int MAIORid, int userAnswer);
+
+// Mostra uma mensagem abaixo da caixa de input
+void Style_underInput(State State);
+
 // Imprime a arte da interface inicial
 void Style_interfaceInicial();
 
 // Printa o menu, retorna visualmente o que o usuário está selecionando e retorna o valor associado a cada opção
 int Style_showMenu();
 
-// Printa uma caixa de input e recebe a resposta do usuário (ideal para resposta em formato de string)
-void Style_input(int option, void *userAnswer, Say Say);
+// Printa uma caixa de input e recebe a resposta do usuário (aceita diferentes formatos)
+void Style_input(void *userAnswer, Say Say);
 
 // Verifica se a tela possui tamanho ideal para execução do código (garante uma exebição agradável dos elementos visuais)
 int Style_executionVerify();
@@ -42,11 +52,11 @@ int Style_executionVerify();
 // Animação inicial de inicialização
 void Style_startProgram();
 
-// Mostra os links
+// Mostra os links entre os sites
 void Style_showLinks();
 
-// Printa o id do site, o nome do site e o seu atual valor de importância (função adaptada para essa biblioteca)
-void Style_showRank(Graph *g);
+// Printa o id do site, o nome do site, o somatório da sua importância e o peso da sua aresta
+void Style_showTable(Graph *g);
 
 // Encontra os sites que possuem a expressão desejada e retorna para a tela
 void Style_searchResult(Graph *g, IndiceInvertido *ind, char *expressao);
